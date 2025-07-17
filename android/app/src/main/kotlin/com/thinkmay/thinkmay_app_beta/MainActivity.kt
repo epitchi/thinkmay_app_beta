c66ca033bbf6bd4791ecce406a1d96dcc30324d0
epitchi <thienvanlea1@gmail.com>
2025-07-17 16:29:22 +0700

    1. Communicate from flutter to native android

--- android/app/src/main/kotlin/com/thinkmay/thinkmay_app_beta/MainActivity.kt
+++ android/app/src/main/kotlin/com/thinkmay/thinkmay_app_beta/MainActivity.kt
@@ -1,6 +1,24 @@
 package com.thinkmay.thinkmay_app_beta
-
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.thinkmay.thinkmay_app_beta/battery"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+                call, result ->
+                if (call.method == "getBatteryLevel") {
+                    result.success("dumb as");
+
+                } else {
+                    result.notImplemented();
+                }
+            // Note: this method is invoked on the main thread.
+            // TODO
+        }
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -56,6 +57,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  static const platform = MethodChannel('com.thinkmay.thinkmay_app_beta/battery');
+
 
   void _incrementCounter() {
     setState(() {
@@ -66,6 +69,25 @@ class _MyHomePageState extends State<MyHomePage> {
       // called again, and so nothing would appear to happen.
       _counter++;
     });
+  }
+
+  // Get battery level.
+  //String _batteryLevel = 'Unknown battery level.';
+
+  Future<void> _getBatteryLevel() async {
+    //String batteryLevel;
+    try {
+      print("test from flutter");
+      final result = await platform.invokeMethod('getBatteryLevel');
+      print(result);
+      // batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      print(e);
+    }
+
+  //  setState(() {
+  //    _batteryLevel = batteryLevel;
+  //  });
   }
 
   @override
@@ -90,19 +112,6 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -116,7 +125,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _getBatteryLevel,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
